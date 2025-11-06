20c0efed3644ed25b8dd36100d59e92bd4cd9f83
10555gff <1073245193@qq.com>
2025-11-06 15:06:45 +0800

    kk

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,8 @@
-
-
-
-
-
 $(function(){
    const $scenBtn  = $('#connect');
- 
- 
- 
    $scenBtn.click(function(){
        console.log('正在请求蓝牙设备...');
        window.deviceAPI.scanBluetoothDevices();
-       //window.services.startScanning();
   });
  
    //...
